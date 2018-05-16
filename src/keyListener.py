ea48698a3a428a8ca6c896420a2b1a224a5c428c
huebnerl <LarsHuebner2@web.de>
2018-05-16 19:42:41 +0200

    Added label append timeout.
    As long as delta time between two keydowns is smaller than 700 millisec the text will get appended to the label.

--- src/keyListener.py
+++ src/keyListener.py
@@ -1,27 +1,38 @@
 from gui import KeyDisplay
 from keyMap import key_map
 
+import time
 import pyHook
 
 class KeyListener:
     
     def __init__(self):
-        self.key_display = KeyDisplay()
+        self.t1 = 0
+        self.t2 = 0
 
         self.hm = pyHook.HookManager()
         self.hm.KeyDown = self.on_keyboard_event
         self.hm.HookKeyboard()
         print("[Programm is listening.]")
 
+        self.key_display = KeyDisplay()
         self.key_display.start()
 
     def on_keyboard_event(self, event):
         print("[ Key pressed: " + event.Key + " ]")
-        
+        self.update_time()
+
         shown_key = event.Key
         if shown_key in key_map:
             shown_key = key_map[shown_key]
 
-        self.key_display.print(shown_key)
+        if (self.t1 - self.t2) > 700:
+            self.key_display.print(shown_key)
+        else:
+            self.key_display.append(shown_key)
         return True
+
+    def update_time(self):
+        self.t2 = self.t1
+        self.t1 = int(round(time.time() * 1000))
 
